d03ea513535351ac73c35f919d8843c092b7e588
MuntasirAbid <muntasira7@hotmail.com>
2023-02-24 22:33:53 +0100

    API created to apply for the job

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const run = async () => {
       res.send(result);
     });
 
+
     app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
 
@@ -48,7 +49,41 @@ const run = async () => {
       res.send({ status: false });
     });
 
+    //apply for the job
+    app.patch("/apply", async (req, res) => {
+      const userId = req.body.userId;
+      const jobId = req.body.jobId;
+      const email = req.body.email;
+
+      const filter = { _id: ObjectId(jobId) };
+      const updateDoc = {
+        $push: { applicants: { id: ObjectId(userId), email } },
+      };
+
+      const result = await jobCollection.updateOne(filter, updateDoc);
+
+      if (result.acknowledged) {
+        return res.send({ status: true, data: result });
+      }
+
+      res.send({ status: false });
+    });
+
     //posting job
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find({});
+      const result = await cursor.toArray();
+      res.send({ status: true, data: result });
+    });
+
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await jobCollection.findOne({ _id: ObjectId(id) });
+      res.send({ status: true, data: result });
+    });
+
     app.post("/job", async (req, res) => {
       const job = req.body;
 
