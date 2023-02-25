acd165b8656fbc8893add77bb23d74583d3681f4
MuntasirAbid <muntasira7@hotmail.com>
2023-02-25 21:51:58 +0100

    API generated for getting applied jobs for users

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ const run = async () => {
     app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
 
-      const result = await userCollection.findOne({ email });
 
+      const result = await userCollection.findOne({ email });
+      console.log(result);
       if (result?.email) {
 
         return res.send({ status: true, data: result });
@@ -69,8 +70,17 @@ const run = async () => {
       res.send({ status: false });
     });
 
-    //posting job
+    //get the applied jobs for a single user
+    app.get("/applied-jobs/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { applicants: { $elemMatch: { email: email } } };
+      const cursor = jobCollection.find(query).project({ applicants: 0 });
+      const result = await cursor.toArray();
 
+      res.send({ status: true, data: result });
+    });
+
+    //posting job
     app.get("/jobs", async (req, res) => {
       const cursor = jobCollection.find({});
       const result = await cursor.toArray();
